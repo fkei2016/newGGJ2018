aa43de3e7997a6592d8e6b397018aa8f8c9d5807
teramoti <33808272+teramoti@users.noreply.github.com>
2018-01-27 16:25:39 +0900

    Titleの弾のフラグ管理しました。

--- GGJ/Assets/Scripts/Title.cs
+++ GGJ/Assets/Scripts/Title.cs
@@ -1,22 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour {
 
+    bool BulletUpFlag;
+    bool BulletDownFlag;
+    bool BulletAFlag;
+    bool BulletLFlag;
+    bool BulletInfinite;
+
     void Start()
     {
-
+        BulletInfinite = false;
+        BulletUpFlag = false;
+        BulletDownFlag = false;
+        BulletAFlag = false;
+        BulletLFlag = false;
     }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("Story1");
         }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            BulletUpFlag = true;
+        }
+       if (BulletUpFlag == true && Input.GetKeyDown(KeyCode.DownArrow))
+       {
+           BulletDownFlag = true;
+       }
+       if (BulletDownFlag == true && Input.GetKeyDown(KeyCode.A))
+       {
+           BulletAFlag = true;
+       }
+       if (BulletAFlag == true && Input.GetKeyDown(KeyCode.L))
+       {
+           BulletLFlag = true;
+       }
 
+        if (BulletLFlag == true)
+        {
+            BulletInfinite = true;
+        }
+
+        if (BulletInfinite == true)
+        {
+            //弾を無限にする処理
+            Debug.Log("aaa");
+        }
     }
-       
+    public bool GetBulletFlag()
+    {
+        return BulletInfinite;
+    }
 }
+
+        
