706a813d7366e97c865870635cbd3649b6b99b84
teramoti <33808272+teramoti@users.noreply.github.com>
2018-01-28 14:27:54 +0900

    Story1のあにめです。プレイに移動

--- GGJ/Assets/Scripts/Story1.cs
+++ GGJ/Assets/Scripts/Story1.cs
@@ -5,25 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Story1 : MonoBehaviour {
 
-    int storyFrame;
-    int storytime;
 	// Use this for initialization
 	void Start () {
-        storyFrame = 0;
-        storytime = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        storyFrame++;
-
-        if (storyFrame > 60)
-        {
-            storytime += 1;
-            storyFrame = 0;
-        }
-
-        if (storytime >= 5)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("Play");
         }
